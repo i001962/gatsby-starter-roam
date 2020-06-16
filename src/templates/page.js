4362bf7b1cc4949cfba51cfee47cc3349b6449fb
I001962 <i001962@gmail.com>
2020-06-16 14:23:14 -0700

    Revert "keep up to date"

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,59 +1,20 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Block from "../components/block"
 
 export default function Page({ data }) {
-  const page = data.roamPage
+  const page = data.roamJson
   return (
     <Layout>
       <h1>{page.title}</h1>
-      <Block parent={page}></Block>
     </Layout>
   )
 }
 
-// Supports 10-levels of nesting.
-// [Why can't it do infinite levels?](https://github.com/graphql/graphql-spec/issues/91#issuecomment-254895093)
 export const query = graphql`
   query($slug: String!) {
-    roamPage(fields: { slug: { eq: $slug } }) {
+    roamJson(fields: { slug: { eq: $slug } }) {
       title
-      children {
-        ...BlockFields
-        children {
-          ...BlockFields
-          children {
-            ...BlockFields
-            children {
-              ...BlockFields
-              children {
-                ...BlockFields
-                children {
-                  ...BlockFields
-                  children {
-                    ...BlockFields
-                    children {
-                      ...BlockFields
-                      children {
-                        ...BlockFields
-                        children {
-                          ...BlockFields
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
     }
-  }
-  fragment BlockFields on RoamBlock {
-    id
-    uid
-    string
   }
 `
